ac64a7c00f9877add3a0704ea4e3817c6436ab00
Sdidt <siddharthms607@gmail.com>
2022-11-06 01:11:01 +0800

    feat: added logic for tracking subquery level

--- postorder.py
+++ postorder.py
@@ -1,42 +1,53 @@
 from __future__ import annotations
-from types import NoneType
+# from types import NoneType
 from dotenv import load_dotenv
 from connection import DB
+from difflib import SequenceMatcher
 
 
 load_dotenv()
 db = DB()
 
+
+decomposed_query = {
+    'subqueries': {'sub_number_1': {'subqueries': {}, 'select': ['sum(partsupp_1.ps_supplycost*partsupp_1.ps_availqty)*0.0001000000'], 'from': ['p1', 's', 'nation'], 'as': {'partsupp': 'P1', 'supplier': 'S'}, 'where': ['P1.ps_suppkey = S.s_suppkey', 'S.s_nationkey = nation_1.n_nationkey', "nation_1.n_name = 'GERMANY'"]}}, 'select': ['partsupp.ps_partkeyps', 'sum(partsupp.ps_supplycost*partsupp.ps_availqty)'], 'as': {'value': 'sum(partsupp.ps_supplycost*partsupp.ps_availqty)', 'partsupp': 'P'}, 'from': ['p', 'supplier', 'nation'], 'where': ['PS.ps_suppkey = supplier.s_suppkey', 'supplier.s_nationkey <> nation.n_nationkey', "nation.n_name = 'GERMANY'", 'P.ps_supplycost > 20', 'supplier.s_acctbal > 10'], 'group by': ['p.ps_partkey'], 'having': ['sum ( P.ps_supplycost * P.ps_availqty ) > sub_number_1'], 'order by': ['sum(partsupp.ps_supplycost*partsupp.ps_availqty)', 'asc']
+    }
+
+query_component_dict = {
+    'subqueries': {'sub_number_1': {'subqueries': {}, 'select': 'select sum ( ps_supplycost * ps_availqty ) * 0.0001000000', '': '', 'from': 'from partsupp P1 , supplier S , nation', 'where': "where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'GERMANY'"}}, 'select': 'select ps_partkey PS , sum ( ps_supplycost * ps_availqty ) as value', '': '', 'from': 'from partsupp P , supplier , nation', 'where': "where PS.ps_suppkey = s_suppkey and not s_nationkey = n_nationkey and n_name = 'GERMANY' and ps_supplycost > 20 and s_acctbal > 10", 'group by': 'group by ps_partkey', 'having': 'having sum ( ps_supplycost * ps_availqty ) >sub_number_1', 'order by': 'order by value ;'
+    }
+
+
 query = """
 select
-      ps_partkey,
-      sum(ps_supplycost * ps_availqty) as value
+    ps_partkey PS,
+    sum(ps_supplycost * ps_availqty) as value
     from
-      partsupp,
-      supplier,
-      nation
+    partsupp P,
+    supplier,
+    nation
     where
-      ps_suppkey = s_suppkey
-      and s_nationkey = n_nationkey
-      and n_name = 'GERMANY'
-      and ps_supplycost > 20
-      and s_acctbal > 10
+    P.ps_suppkey = s_suppkey
+    and not s_nationkey = n_nationkey
+    and n_name = 'GERMANY'
+    and ps_supplycost > 20
+    and s_acctbal > 10
     group by
-      ps_partkey having
+    ps_partkey having
         sum(ps_supplycost * ps_availqty) > (
-          select
+        select
             sum(ps_supplycost * ps_availqty) * 0.0001000000
-          from
-            partsupp,
-            supplier,
+        from
+            partsupp P1,
+            supplier S,
             nation
-          where
-            not ps_suppkey < s_suppkey
+        where
+            ps_suppkey = s_suppkey
             and s_nationkey = n_nationkey
-            and n_name = 'AUSTRIA'
-        )
+            and n_name = 'GERMANY'
+        ) 
     order by
-      value desc;
+    value;
 """
 
 # query = """
@@ -66,8 +77,9 @@ dct = analyze_fetched[0][0][0]["Plan"]
 
 
 class Node():
-    def __init__(self, data: dict) -> None:
+    def __init__(self, data: dict, subquery_level) -> None:
         self.type = data.get("Node Type")
+        self.subquery_level = subquery_level
         self.parent = []
         self.children = []
         self.information = None
@@ -165,29 +177,45 @@ class Node():
     def node_hash_join(self):
         relevant_info = {}
         condition = self.information['Hash Cond'][1:-1]
-        relevant_info["WHERE"] = condition
-        
+        relevant_info["where"] = condition
+        original_query_component = query_component_dict['where']
+        for k, v in relevant_info.items():
+            print("blahblahblah")
+            similarity_score = 0
+            for clause in decomposed_query[k]:
+                print(clause)
+                if similarity_score < SequenceMatcher(None, clause, v).ratio():
+                    similarity_score = SequenceMatcher(None, clause, v).ratio()
+                    print(similarity_score)
+            if similarity_score > 0.65:
+                print("Fairly good match is found")
+        print(original_query_component)
+        print("The condition " + condition + " is implemented using hash join because ...")
         return relevant_info
     
 
 
 
-def capture_nodes(dct, parent):
+def capture_nodes(dct, parent, subquery_level=0):
 
-    cur = Node(dct)
+    cur = Node(dct, subquery_level)
     # print("Current Node Type: {}".format(type(cur)))
 
     if "Plans" in dct:
         plans = dct["Plans"]
         for plan in plans:
+
             dct = plan
-            capture_nodes(dct, cur)
+            if "Subplan Name" not in dct:
+                capture_nodes(dct, cur, subquery_level)
+            else:
+                capture_nodes(dct, cur, subquery_level + 1)
 
     cur.add_parent(parent)
     nodes.append(cur)
 
     # update cur node as child of its parent
-    if not isinstance(cur.parent[0], NoneType):
+    if not cur.parent[0] is None:
         cur.parent[0].add_child(cur)
 
 
@@ -206,8 +234,9 @@ for node in nodes:
     print("MAPPING: {}".format(node.mapping()))
     # print("RELEVANT INFORMATION: \n{}".format(node.get_relevant_info()))
     print("OTHER INFORMATION: {}".format(node.information))
-    if not isinstance(node.parent[0], NoneType):
+    if not node.parent[0] is None:
         print("PARENT NODE TYPE: {}".format(node.parent[0].type))
+    print("SUBQUERY LEVEL: {}".format(node.subquery_level))
     
     children = node.children
     if children:
