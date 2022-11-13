37292e55f7e114955ececbef9ff57412758cb34b
Sdidt <siddharthms607@gmail.com>
2022-11-13 13:46:35 +0800

    feat: completed commenting on interface.py

--- interface.py
+++ interface.py
@@ -330,6 +330,17 @@ app.layout = html.Div(children=[
 ])
 
 def recursive_display(dict_output, div_components: list = [], title_padding=0, table_padding=1):
+    """
+    Procedure to recursively annotated output and display in a formatted manner
+
+    Parameter dict_output: dictionary output from annotator
+
+    Parameter div_components: current list of div components for displaying 
+
+    Parameter title_padding: current title padding (for current query level)
+
+    Parameter table_padding: current table padding (for current query level)
+    """
     if (dict_output == {}):
         print("Base case")
         return [], div_components
@@ -345,20 +356,13 @@ def recursive_display(dict_output, div_components: list = [], title_padding=0, t
         div_components.append(div_component)
         divider = html.Div(className="vl", style={'width':'max-width', 'border-top':'2px solid black', 'padding-right': 10})
         div_components.append(divider)
-        # print("After adding title and divider")
-        # print(div_components)
-        # dict_object = {'Query': str(dict_keys[i]), 'Natural Language Description': str(dict_keys[i])}
-        # dict_array.append(dict_object)
         nested_dict = dict_values[i]
-        # print(list(nested_dict.keys()))
         nested_dict_keys = list(nested_dict.keys())
         nested_dict_values = list(nested_dict.values())
         subquery_dict_array = []
         for j in range(len(nested_dict_keys)):
-            # print(nested_dict_keys[j])
             if (nested_dict_keys[j] == "subqueries"):
                 subquery_dict_array, div_components = recursive_display(nested_dict_values[j], div_components, title_padding + 10, table_padding + 10)
-                # print(div_components)
             else:
                 dict_temp_array = nested_dict_values[j]
                 dict_temp_str = ""
@@ -368,8 +372,6 @@ def recursive_display(dict_output, div_components: list = [], title_padding=0, t
                 dict_object = {'Query': str(nested_dict_keys[j]), 'Natural Language Description': str(dict_temp_str)}
                 dict_array.append(dict_object)
         print(dict_array)
-        # print("before adding datatable")
-        # print(div_components)
         div_component = dash_table.DataTable(data=dict_array, 
                 style_table={
                     'padding-left': table_padding, 'padding-top': 10, 'padding-bottom': 10,
@@ -384,6 +386,9 @@ def recursive_display(dict_output, div_components: list = [], title_padding=0, t
     return dict_array, div_components 
 
 def toggle_modal(n1, is_open):
+    """
+    Function to control toggling of accordion sections
+    """
     if n1:
         return not is_open
     return is_open
@@ -436,13 +441,15 @@ app.callback(
     Output("modal-datasets", "is_open"), Input("datasetsLink", "n_clicks"), State("modal-datasets", "is_open"),
 )(toggle_modal)
 
-""" Call back for Connection to data server """
 @app.callback(
     Output('modal-database', 'is_open'), Output('port-error-message', 'is_open'), Input('dbdetails_submit_button', 'n_clicks'),
     State('host-input', 'value'), State('dbname-input', 'value'), State('username-input', 'value'),
     State('password-input', 'value'), State('port-input', 'value')
 )
 def get_database_inputs(n_clicks, host, dbname, username, password, port):
+    """
+    Callback to validate database inputs
+    """
     if n_clicks:
         try:
             port = int(port)
@@ -452,13 +459,14 @@ def get_database_inputs(n_clicks, host, dbname, username, password, port):
         return True, False
 
 
-""" Call back for Loading of Page when submit SQL Query """
 @app.callback(
     Output('modal-loading', 'is_open'), Input('query_submit_button', 'n_clicks'),
     Input('textarea-sql-query', 'value'), State('loading-boolean', 'children')
 )
-
 def open_loading(n_clicks, value, loading):
+    """ 
+    Call back for Loading of Page when submit SQL Query 
+    """
     print(ctx.inputs)
     # print(error)
     # print(str(ctx.triggered_id))
@@ -479,19 +487,9 @@ def open_loading(n_clicks, value, loading):
 )
 
 def update_output(n_clicks, value, host, dbname, username, password, port):
-    # print(open)
-    # open_loading(n_clicks, value, "", "")
-    # print(value)
-    # print(n_clicks)
-    # print("QUERY OUTPUT: {}".format(query_output))
-    # html_output = []
-    # dict_output = {'query_1': {'subqueries': 
-    #     {'subquery_1': {'subqueries': {}, 'from partsupp P1 , supplier S , nation': ['The clause "from p1" is implemented using Seq Scan because no other option is available.', 'The clause "from s" is implemented using Seq Scan because it requires 44.62, 10351967.39 less operations than Bitmap Heap Scan, Index Scan respectively.', 'The clause "from nation_1" is implemented using Seq Scan because it requires 2.90, 277777779.19 less operations than Bitmap Heap Scan, Index Scan respectively.'], "where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'GERMANY'": ['The clause "where nation_1.n_name = \'GERMANY\'" is implemented using Seq Scan because it requires 2.90, 277777779.19 less operations than Bitmap Heap Scan, Index Scan respectively.', 'The clause "where s.s_nationkey = nation_1.n_nationkey" is implemented using Hash Join.', 'The clause "where p1.ps_suppkey = s.s_suppkey" is implemented using Hash Join.']}}
-    #     , 'from partsupp P , supplier , nation': ['The clause "from p" is implemented using Seq Scan because no other option is available.', 'The clause "from nation" is implemented using Seq Scan because no other option is available.', 'The clause "from supplier" is implemented using Seq Scan because it requires 124.21, 28818445.30 less operations than Bitmap Heap Scan, Index Scan respectively.'], "where P.ps_suppkey = s_suppkey and not s_nationkey = n_nationkey and n_name = 'GERMANY' and ps_supplycost > 20 and s_acctbal > 10": ['The clause "where p.ps_supplycost > \'20\'" is implemented using Seq Scan because no other option is available.', 'The clause "where nation.n_name = \'GERMANY\'" is implemented using Seq Scan because no other option is available.', 'The clause "where supplier.s_acctbal > \'10\'" is implemented using Seq Scan because it requires 124.21, 28818445.30 less operations than Bitmap Heap Scan, Index Scan respectively.', 'The clause "where supplier.s_nationkey <> nation.n_nationkey" is implemented using Nested Loop.', 'The clause "where p.ps_suppkey = supplier.s_suppkey" is implemented using Hash Join.'], 'order by value ;': ['The clause "order by sum((p.ps_supplycost * (p.ps_availqty) asc" is implemented using external merge Sort.']
-    #     }
-    # }
-    # set_output = {'query_1': {'subqueries': {}, 'select': 'select n_nationkey', 'from': 'from nation', 'intersect': 'intersect query_2'}, 'query_2': {'subqueries': {}, 'select': 'select s_nationkey', 'from': 'from supplier'}}
-    
+    """
+    Callback to update output on screen for new query
+    """
     if n_clicks > 0:
         print("HOST: {}".format(host))
         print("DB NAME: {}".format(dbname))
@@ -499,12 +497,6 @@ def update_output(n_clicks, value, host, dbname, username, password, port):
         print("PASSWORD: {}".format(password))
         print("PORT: {}".format(port))
         query_output = process_query(value, host, dbname, username, password, port)
-        # query_output = {}
-        #     query_output = {'query_1': {'subqueries': 
-        #     {'subquery_1': {'subqueries': {}, 'from partsupp P1 , supplier S , nation': ['The clause "from p1" is implemented using Seq Scan because no other option is available.', 'The clause "from s" is implemented using Seq Scan because it requires 44.62, 10351967.39 less operations than Bitmap Heap Scan, Index Scan respectively.', 'The clause "from nation_1" is implemented using Seq Scan because it requires 2.90, 277777779.19 less operations than Bitmap Heap Scan, Index Scan respectively.'], "where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'GERMANY'": ['The clause "where nation_1.n_name = \'GERMANY\'" is implemented using Seq Scan because it requires 2.90, 277777779.19 less operations than Bitmap Heap Scan, Index Scan respectively.', 'The clause "where s.s_nationkey = nation_1.n_nationkey" is implemented using Hash Join.', 'The clause "where p1.ps_suppkey = s.s_suppkey" is implemented using Hash Join.']}}
-        #     , 'from partsupp P , supplier , nation': ['The clause "from p" is implemented using Seq Scan because no other option is available.', 'The clause "from nation" is implemented using Seq Scan because no other option is available.', 'The clause "from supplier" is implemented using Seq Scan because it requires 124.21, 28818445.30 less operations than Bitmap Heap Scan, Index Scan respectively.'], "where P.ps_suppkey = s_suppkey and not s_nationkey = n_nationkey and n_name = 'GERMANY' and ps_supplycost > 20 and s_acctbal > 10": ['The clause "where p.ps_supplycost > \'20\'" is implemented using Seq Scan because no other option is available.', 'The clause "where nation.n_name = \'GERMANY\'" is implemented using Seq Scan because no other option is available.', 'The clause "where supplier.s_acctbal > \'10\'" is implemented using Seq Scan because it requires 124.21, 28818445.30 less operations than Bitmap Heap Scan, Index Scan respectively.', 'The clause "where supplier.s_nationkey <> nation.n_nationkey" is implemented using Nested Loop.', 'The clause "where p.ps_suppkey = supplier.s_suppkey" is implemented using Hash Join.'], 'order by value ;': ['The clause "order by sum((p.ps_supplycost * (p.ps_availqty) asc" is implemented using external merge Sort.']
-        #     }
-        # }
         if "connection-error" in query_output:
             return html.Div([]), html.Div([]), "false", False, True
         if (query_output == {}):
@@ -514,9 +506,6 @@ def update_output(n_clicks, value, host, dbname, username, password, port):
             div_components = []
             lst_curr_object, div_components = recursive_display(query_output, [])
             html_output = div_components
-            
-            # print("FINAL RESULT: {}".format(div_components))
-            # print("HTML OUTPUT: {}".format(html_output))
             print("done executing")
             # print(html_output)
             return html.Div(html_output), [dbc.Modal([
